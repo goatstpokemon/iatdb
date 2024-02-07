77c9b830a8261a40ec35ab8c48b46ec1c817d1d7
Luke Dixon <github@lukedixon.nl>
2024-02-07 14:43:09 +0100

    init commit

---
+++ resources/js/app.jsx
@@ -0,0 +1,9 @@
+
+import ReactDOM from 'react-dom/client';
+import '../css/app.css';
+import Home from '../views/Home';
+
+ReactDOM.createRoot(document.getElementById('app')).render(
+    <Home />
+);
+
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+   content: [
+    "./resources/**/*.jsx",
+    "./resources/**/*.tsx",
+    "./resources/**/*.js",
+    "./resources/**/*.blade.php",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
